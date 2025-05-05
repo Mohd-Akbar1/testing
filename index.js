d5a0b7a07efd938d59a4ece21ce14d9ceda0f656
Mohd-Akbar1 <mohd.akbar@thewasserstoff.com>
2025-05-05 15:37:09 +0530

    main

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const express=require('express');
-const app=express();
-
-
-app.get('/',function(req,res){
-    res.send('render via versel')
-})
-app.listen(8000,function(){
-    console.log('Server is running')
-})
+console.log('hello world')
